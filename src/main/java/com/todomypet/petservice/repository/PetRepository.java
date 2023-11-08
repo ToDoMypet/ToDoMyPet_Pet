54070a1ee41e82feb3e5a2a87b150f4188314fde
dawonseo <oloielo@naver.com>
2023-11-08 09:56:43 +0900

    chore: pet repository 쿼리 삭제

--- src/main/java/com/todomypet/petservice/repository/PetRepository.java
+++ src/main/java/com/todomypet/petservice/repository/PetRepository.java
@@ -1,7 +1,6 @@
 package com.todomypet.petservice.repository;
 
 import com.todomypet.petservice.domain.node.Pet;
-import com.todomypet.petservice.dto.GetAdoptedPetDTO;
 import org.springframework.data.neo4j.repository.Neo4jRepository;
 import org.springframework.data.neo4j.repository.config.EnableNeo4jRepositories;
 import org.springframework.data.neo4j.repository.query.Query;
@@ -12,12 +11,6 @@ import java.util.List;
 @EnableNeo4jRepositories
 @Repository
 public interface PetRepository extends Neo4jRepository<Pet, String> {
-
-    @Query("MATCH (u:User{id:$userId}) WITH u " +
-            "MATCH (u)-[a:ADOPT]->(p:Pet) WHERE (p.grade = 'ADULT' AND a.graduated = true) OR (a.graduated = false) " +
-            "RETURN p ORDER BY a.seq")
-    List<Pet> getAdoptedPetList(String userId);
-
     @Query("MATCH (u:User)-[a:ADOPT]->(p:Pet) " +
             "WHERE a.seq = $seq RETURN p")
     Pet getPetBySeqOfAdopt(String seq);
